b48e007c05339bd1fdff0e9f640cd600cbf48b01
ilsong963 <ilsong963@naver.com>
2024-09-03 20:16:11 +0900

    StatefulShellRoute 수정

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_instargram_clone/presentation/bottom_nav_bar.dart';
 import 'package:flutter_instargram_clone/presentation/screen/home/home_screen.dart';
 import 'package:flutter_instargram_clone/presentation/screen/post_screen.dart';
 import 'package:flutter_instargram_clone/presentation/screen/profile/profile_screen.dart';
@@ -14,18 +13,79 @@ final GoRouter router = GoRouter(
   navigatorKey: _rootNavigatorKey,
   initialLocation: '/home',
   routes: [
-    ShellRoute(
-      navigatorKey: _shellNavigatorKey,
-      builder: (context, state, child) {
-        return BottomNavBar(child: child);
+    StatefulShellRoute.indexedStack(
+      builder: (BuildContext context, GoRouterState state, StatefulNavigationShell navigationShell) {
+        return Scaffold(
+          body:SafeArea(child:  navigationShell),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: 'Post',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.video_collection),
+                label: 'reels',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            currentIndex: navigationShell.currentIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: (int index) {
+
+            // 브랜치를 전환하는데는 StatefulNavigationShell.goBranch 메서드를 사용한다.
+            navigationShell.goBranch( index);
+          },
+
+            unselectedItemColor: Colors.grey,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+
+          ),
+        );
       },
-      routes: [
-        // This screen is displayed on the ShellRoute's Navigator.
-        GoRoute(path: '/home', pageBuilder: (context, state) =>  NoTransitionPage(child: HomeScreen())),
-        GoRoute(path: '/search', pageBuilder: (context, state) => const NoTransitionPage(child: SearchScreen())),
-        GoRoute(path: '/post', pageBuilder: (context, state) => const NoTransitionPage(child: PostScreen())),
-        GoRoute(path: '/reels', pageBuilder: (context, state) => const NoTransitionPage(child: ReelsScreen())),
-        GoRoute(path: '/profile', pageBuilder: (context, state) => const NoTransitionPage(child: ProfileScreen())),
+      branches: <StatefulShellBranch>[
+// Tab A (First Tab)
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            // This screen is displayed on the ShellRoute's Navigator.
+            GoRoute(path: '/home', pageBuilder: (context, state) => const NoTransitionPage(child: HomeScreen()))
+          ],
+        ),
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            // This screen is displayed on the ShellRoute's Navigator.
+            GoRoute(path: '/search', pageBuilder: (context, state) => const NoTransitionPage(child: SearchScreen())),
+          ],
+        ),   StatefulShellBranch(routes: <RouteBase>[
+          // This screen is displayed on the ShellRoute's Navigator.
+          GoRoute(path: '/post', pageBuilder: (context, state) => const NoTransitionPage(child: PostScreen())),
+        ]),
+
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            // This screen is displayed on the ShellRoute's Navigator.
+            GoRoute(path: '/reels', pageBuilder: (context, state) => const NoTransitionPage(child: ReelsScreen())),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: <RouteBase>[
+            // This screen is displayed on the ShellRoute's Navigator.
+            GoRoute(path: '/profile', pageBuilder: (context, state) => const NoTransitionPage(child: ProfileScreen())),
+          ],
+        ),
       ],
     ),
   ],
