fcfe650fff9e45a8fdd633d405abe02aa6adb399
ilsong963 <ilsong963@naver.com>
2024-09-03 20:16:21 +0900

    search bar 높이 수정

--- lib/presentation/screen/search/content_search_bar.dart
+++ lib/presentation/screen/search/content_search_bar.dart
@@ -8,17 +8,22 @@ class ContentSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search',
-          contentPadding: const EdgeInsets.all(0),
-          prefixIcon: const Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
+      padding: const EdgeInsets.all(5.0),
+      child:Container(
+        height: 45,
+        child:  TextField(
+
+          decoration: InputDecoration(
+
+            hintText: 'Search',
+            contentPadding: const EdgeInsets.all(0),
+            prefixIcon: const Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
